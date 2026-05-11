9f336955ac8ad8cb3e9965afe84eff9fb242533f
cristian038 <133276168+crisitian038@users.noreply.github.com>
2026-05-11 13:31:42 -0600

    todo

---
+++ service-worker.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'agenda-pwa-v1';
+const URLS_TO_CACHE = [
+  './',
+  './agenda.html',
+  './manifest.json',
+  './service-worker.js',
+  './icons/icon.svg',
+  'https://cdn.jsdelivr.net/npm/pouchdb@8.0.1/dist/pouchdb.min.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames
+          .filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) return cachedResponse;
+      return fetch(event.request)
+        .then((networkResponse) => {
+          return caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          });
+        })
+        .catch(() => caches.match('./agenda.html'));
+    })
+  );
+});
